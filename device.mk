854ad34c746a7d04714a194c43700dcddb35822f
yograjfire18 <yograjfire18@gmail.com>
2023-08-17 17:38:40 +0530

    spartan: init: Add some missing inits
    Signed-off-by: yograjfire18 <yograjfire18@gmail.com>

--- device.mk
+++ device.mk
@@ -256,7 +256,9 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.spartan.rc \
     init.target.rc \
+    oOFTweaks.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc
 
