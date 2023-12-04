d6a0419280fec70964252b26f74b2a9767d49256
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2023-12-04 17:13:18 +0530

    spartan: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 echo 'Cloning vendor'
-git clone https://github.com/RealmeGTNeo3T-Devs/proprietary_vendor_realme_spartan.git -b derp-14 vendor/realme/spartan
+git clone https://github.com/RealmeGTNeo3T-Devs/proprietary_vendor_realme_spartan.git -b dragon-14 vendor/realme/spartan
 
 echo "Cloning kernel"
 git clone https://github.com/RealmeGTNeo3T-Devs/android_kernel_realme_sm8250.git kernel/realme/sm8250 --depth=1
 
 echo "Cloning oplus hals"
-git clone https://github.com/RealmeGTNeo3T-Devs/android_hardware_oplus.git hardware/oplus -b derp-14
+git clone https://github.com/RealmeGTNeo3T-Devs/android_hardware_oplus.git hardware/oplus -b test-14
