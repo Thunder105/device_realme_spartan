1b4c1496ebb0ac30d62ccec3c6269a6737577705
Thunder105 <hariprakashmurugesan@gmail.com>
2024-03-11 10:08:35 +0530

    spartan: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,14 @@
 echo 'Cloning vendor'
-git clone https://github.com/Dragon-s-Playground/android_vendor_realme_spartan.git -b dragon-test vendor/realme/spartan
+git clone https://github.com/Thunder105/vendor_realme_spartan.git  vendor/realme/spartan
 
 echo "Cloning kernel"
-git clone https://github.com/RealmeGTNeo3T-Devs/android_kernel_realme_sm8250.git kernel/realme/sm8250 --depth=1
+git clone https://github.com/Thunder105/14_kernel_realme_sm8250.git kernel/realme/sm8250 --depth=1
 
 echo "Cloning oplus hals"
-git clone https://github.com/RealmeGTNeo3T-Devs/android_hardware_oplus.git hardware/oplus -b test-14
+git clone https://github.com/Thunder105/hardware_oplus.git hardware/oplus 
+
+echo "Cloning Go Gcam"
+git clone  https://github.com/Vodkamaniac-rmx3371/packages_apps_GCamGOPrebuilt.git  packages/apps/GCamGOPrebuilt
+
+echo "Cloning BCR"
+git clone https://github.com/Vodkamaniac-rmx3371/vendor_bcr.git vendor/bcr
